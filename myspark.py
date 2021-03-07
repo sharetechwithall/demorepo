f20661818b9b7f8c536238b012d56714c926e68c
ShareTech <sharetechwithall@gmail.com>
2021-03-07 21:20:51 +0530

    test 8

---
+++ myspark.py
@@ -0,0 +1,8 @@
+from pyspark.sql import SparkSession
+from functools import lru_cache
+
+def get_spark():
+    return (SparkSession.builder
+                .master("local")
+                .appName("myapp")
+                .getOrCreate())
